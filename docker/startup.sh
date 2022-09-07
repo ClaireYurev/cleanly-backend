3d6517ae715b7157cdd9ca84fc31819946df1fd0
yamidduarte <yamid9@gmail.com>
2022-09-07 12:48:01 -0700

    Update

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,7 +4,7 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
-php artisan passport:install
+php artisan passport:install --force
 
 #while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 

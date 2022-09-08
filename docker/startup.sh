13684525d66bbc6073c8eb18f1b4c8bd35fffa26
yamidduarte <yamid9@gmail.com>
2022-09-08 02:07:22 -0700

    New

--- docker/startup.sh
+++ docker/startup.sh
@@ -10,9 +10,9 @@ php artisan migrate
 
 php artisan passport:install
 
-php artisan passport:keys
+#php artisan passport:keys
 
-php artisan config:clear
+#php artisan config:clear
 
 #while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
